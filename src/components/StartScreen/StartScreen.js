01b7f032df3f5f4cfb5a164b476e9dc96254c00e
Peter Wagner <peter.wagner1986@gmail.com>
2022-01-31 14:01:38 +0100

    spellcheck

--- src/components/StartScreen/StartScreen.js
+++ src/components/StartScreen/StartScreen.js
@@ -3,7 +3,7 @@ import './StartScreen.css';
 export const StartScreen = ( { startScreen }) => {
   return <div className={ `start-screen flex-center ${ startScreen }` }>
 
-      <h1>Welcome to my Paig</h1>
+      <h1>Welcome to my Paige</h1>
 
   </div>;
 };
